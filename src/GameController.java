bb6ae3c25bed84c5702b2b2d944d2d0a062c2389
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-05-04 13:28:47 -0700

    Changed to Javadoc comments

--- src/GameController.java
+++ src/GameController.java
@@ -1,24 +1,45 @@
+/**
+ * This class represents the game controller for the game.
+ * It manages the game flow and player turns.
+ */
 class GameController {
     private int numPlayers;
     private int days;
 
+    /**
+     * Constructs a new GameController with the specified number of players.
+     *
+     * @param numPlayers the number of players in the game
+     */
     GameController(int numPlayers) {
         this.numPlayers = numPlayers;
         // TODO: Initialize other attributes
     }
 
+    /**
+     * Starts the game and manages the game flow.
+     */
     void playGame() {
         // TODO: Implement game play logic
     }
 
+    /**
+     * Initializes the game with the necessary settings and players.
+     */
     void initGame() {
         // TODO: Implement game initialization logic
     }
 
+    /**
+     * Manages a player's turn in the game.
+     */
     void playerTurn() {
         // TODO: Implement player turn logic
     }
 
+    /**
+     * Ends the game and displays the final results.
+     */
     void endGame() {
         // TODO: Implement game end logic
     }
