486d3150803e67119623392ab6a43b69d78ceabb
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-05-04 13:21:18 -0700

    Initial Deck.java

---
+++ src/Deck.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+/**
+ * Represents a deck of SceneCard objects.
+ */
+public class Deck {
+    private List<SceneCard> cards;
+
+    /**
+     * Initializes a new Deck with SceneCard objects created from the data in the XML file at the given path.
+     * The cards are then shuffled.
+     *
+     * @param xmlFilePath The path to the XML file containing the card data.
+     */
+    Deck(String xmlFilePath) {
+        this.cards = new ArrayList<>();
+        // TODO: Initialize SceneCards and Roles from XML file
+        this.shuffle();
+    }
+
+
+    /**
+     * Removes and returns the top card from the deck.
+     *
+     * @return The top card from the deck.
+     * @throws IllegalStateException If the deck is empty.
+     */
+    SceneCard drawCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(cards.size() - 1);
+        } else {
+            throw new IllegalStateException("Deck is empty");
+        }
+    }
+
+    /**
+     * Shuffles the cards in the deck.
+     */
+    void shuffle() {
+        Collections.shuffle(cards);
+    }
+    
+    /**
+     * Returns true if the deck is empty, false otherwise.
+     */
+    boolean isEmpty() {
+        return cards.isEmpty();
+    }
+}
