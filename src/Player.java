88016ca5740d86ed19c1449d57269ca2305158c1
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-05-04 11:14:08 -0700

    Initial Player.java

---
+++ src/Player.java
@@ -0,0 +1,88 @@
+public class Player {
+    private int id;
+    private int rank;
+    private int money;
+    private int credits;
+    private String locationName;
+    private int rehearsalTokens;
+    private boolean hasRole;
+    private boolean onCard;
+
+    Player(int credits, int rank) {
+        this.credits = credits;
+        this.rank = rank;
+        // TODO: Initialize other attributes as needed
+    }
+
+    int getID() {
+        // TODO: Implement logic to return id
+        return 0;
+    }
+
+    int getRank() {
+        // TODO: Implement logic to return rank
+        return 0;
+    }
+
+    int getMoney() {
+        // TODO: Implement logic to return money
+        return 0;
+    }
+
+    int getCredits() {
+        // TODO: Implement logic to return credits
+        return 0;
+    }
+
+    String getLocation() {
+        // TODO: Implement logic to return locationName
+        return null;
+    }
+
+    int getRehearsalToken() {
+        // TODO: Implement logic to return rehearsalTokens
+        return 0;
+    }
+
+    boolean getHasRole() {
+        // TODO: Implement logic to return hasRole
+        return false;
+    }
+
+    boolean getOnCard() {
+        // TODO: Implement logic to return onCard
+        return false;
+    }
+
+    void setID(int id) {
+        // TODO: Implement logic to set id
+    }
+
+    void setRank(int rank) {
+        // TODO: Implement logic to set rank
+    }
+
+    void setMoney(int money) {
+        // TODO: Implement logic to set money
+    }
+
+    void setCredits(int credits) {
+        // TODO: Implement logic to set credits
+    }
+
+    void setLocation(String locationName) {
+        // TODO: Implement logic to set locationName
+    }
+
+    void setRehearsalToken(int rehearsalTokens) {
+        // TODO: Implement logic to set rehearsalTokens
+    }
+
+    void setHasRole(boolean hasRole) {
+        // TODO: Implement logic to set hasRole
+    }
+
+    void setOnCard(boolean onCard) {
+        // TODO: Implement logic to set onCard
+    }
+}
