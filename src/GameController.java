bfdb8dcb3543d0ca218dabb63b9084c4dc1e5cdb
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-05-04 04:25:24 -0700

    Initial GameController.java

---
+++ src/GameController.java
@@ -0,0 +1,25 @@
+class GameController {
+    private int numPlayers;
+    private int days;
+
+    GameController(int numPlayers) {
+        this.numPlayers = numPlayers;
+        // TODO: Initialize other attributes
+    }
+
+    void playGame() {
+        // TODO: Implement game play logic
+    }
+
+    void initGame() {
+        // TODO: Implement game initialization logic
+    }
+
+    void playerTurn() {
+        // TODO: Implement player turn logic
+    }
+
+    void endGame() {
+        // TODO: Implement game end logic
+    }
+}
