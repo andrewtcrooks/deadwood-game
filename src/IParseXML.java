353389cc19fe2b6ab58b44630305dc198180c5d3
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-05-19 20:44:21 -0700

    updated comments

--- src/IParseXML.java
+++ src/IParseXML.java
@@ -3,6 +3,9 @@ import org.w3c.dom.*;
 import java.io.*;
 import org.xml.sax.SAXException;
 
+/**
+ * Represents an interface for parsing XML files.
+ */
 public interface IParseXML {
     Document getDocFromFile(String filename) throws ParserConfigurationException, IOException, SAXException;
     void readData(Document d);
