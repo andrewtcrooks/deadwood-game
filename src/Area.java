57fc0cf9cf6ff77a069e23cc9982c4ef058e81b0
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-05-21 23:00:25 -0700

    updated comments

--- src/Area.java
+++ src/Area.java
@@ -1,9 +1,22 @@
+
+/**
+ * Area class represents a rectangular area on the board.
+ * 
+ */
 public class Area {
     private int x;
     private int y;
     private int h;
     private int w;
 
+    /**
+     * Initializes a new Area with the given x, y, h, and w.
+     *
+     * @param x The x-coordinate of the Area.
+     * @param y The y-coordinate of the Area.
+     * @param h The height of the Area.
+     * @param w The width of the Area.
+     */
     public Area(int x, int y, int h, int w) {
         this.x = x;
         this.y = y;
@@ -11,18 +24,38 @@ public class Area {
         this.w = w;
     }
 
+    /**
+     * Returns the x-coordinate of the Area.
+     *
+     * @return The x-coordinate of the Area.
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Returns the y-coordinate of the Area.
+     *
+     * @return The y-coordinate of the Area.
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Returns the height of the Area.
+     *
+     * @return The height of the Area.
+     */
     public int getH() {
         return h;
     }
 
+    /**
+     * Returns the width of the Area.
+     *
+     * @return The width of the Area.
+     */
     public int getW() {
         return w;
     }
