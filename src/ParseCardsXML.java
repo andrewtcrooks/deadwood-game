b98b9eba47858dc9eb868ec1b33c6c2ab2ee6c91
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-05-22 00:09:32 -0700

    added area handling

--- src/ParseCardsXML.java
+++ src/ParseCardsXML.java
@@ -29,22 +29,44 @@ public class ParseCardsXML extends AbstractParseXML {
     public void readData(Document d) throws Exception {
         Element root = d.getDocumentElement();
         NodeList cardsList = root.getElementsByTagName("card");
+        parseCards(cardsList);
+    }
+    
+    /**
+     * Parses the scene cards from the given NodeList object.
+     * 
+     * @param cardsList
+     */
+    private void parseCards(NodeList cardsList) {
         for (int i = 0; i < cardsList.getLength(); i++) {
             Node card = cardsList.item(i);
             SceneCard newCard = parseCard((Element) card);
             cards.add(newCard);
         }
     }
-    
+
+    /**
+     * Parses a scene card from the given Element object.
+     * 
+     * @param cardElement
+     * @return SceneCard
+     */
     private SceneCard parseCard(Element cardElement) {
-        String title = cardElement.getAttribute("name");
-        String image = cardElement.getAttribute("img");
-        int budget = Integer.parseInt(cardElement.getAttribute("budget"));
+
+        // Parse name
+        String title = parseName(cardElement);
+
+        // Parse image
+        String image = parseImage(cardElement);
+
+        // Parse budget
+        int budget = parseBudget(cardElement);
     
-        // Parse scene
-        Element sceneElement = (Element) cardElement.getElementsByTagName("scene").item(0);
-        int id = Integer.parseInt(sceneElement.getAttribute("number"));
-        String desc = sceneElement.getTextContent().trim();
+        // Parse scene ID
+        int id = parseSceneID(cardElement);
+
+        // Parse scene description
+        String desc = parseSceneDescription(cardElement);
     
         // Parse roles
         List<Role> roles = parseRoles(cardElement);
@@ -53,6 +75,68 @@ public class ParseCardsXML extends AbstractParseXML {
         return new SceneCard(id, title, desc, image, budget, roles);
     }
     
+    /**
+     * Parses the name from the given Element object.
+     * 
+     * @param cardElement
+     * @return String
+     */
+    private String parseName(Element cardElement) {
+        return cardElement.getAttribute("name");
+    }
+
+    /**
+     * Parses the image from the given Element object.
+     * 
+     * @param cardElement
+     * @return String
+     */
+    private String parseImage(Element cardElement) {
+        return cardElement.getAttribute("img");
+    }
+
+    /**
+     * Parses the budget from the given Element object.
+     * 
+     * @param cardElement
+     * @return int
+     */
+    private int parseBudget(Element cardElement) {
+        return Integer.parseInt(cardElement.getAttribute("budget"));
+    }
+
+    /**
+     * Parses the scene ID from the given Element object.
+     * 
+     * @param cardElement
+     * @return int
+     */
+    private int parseSceneID(Element cardElement) {
+        Element sceneElement = (Element) cardElement.getElementsByTagName("scene").item(0);
+        int id = Integer.parseInt(sceneElement.getAttribute("number"));
+        String desc = sceneElement.getTextContent().trim();
+        return id;
+    }
+
+    /**
+     * Parses the scene description from the given Element object.
+     * 
+     * @param cardElement
+     * @return String
+     */
+    private String parseSceneDescription(Element cardElement) {
+        Element sceneElement = (Element) cardElement.getElementsByTagName("scene").item(0);
+        int id = Integer.parseInt(sceneElement.getAttribute("number"));
+        String desc = sceneElement.getTextContent().trim();
+        return desc;
+    }
+
+    /**
+     * Parses the roles from the given Element object.
+     * 
+     * @param cardElement
+     * @return List<Role>
+     */
     private List<Role> parseRoles(Element cardElement) {
         List<Role> roles = new ArrayList<>();
         NodeList partsList = cardElement.getElementsByTagName("part");
@@ -64,22 +148,46 @@ public class ParseCardsXML extends AbstractParseXML {
         return roles;
     }
     
+    /**
+     * Parses a role from the given Element object.
+     * 
+     * @param partElement
+     * @return Role
+     */
     private Role parseRole(Element partElement) {
         String roleName = partElement.getAttribute("name");
         int roleLevel = Integer.parseInt(partElement.getAttribute("level"));
     
+        // Parse the Area
+        Element areaElement = (Element) partElement.getElementsByTagName("area").item(0);
+        Area roleArea = parseArea(areaElement);
+
         // Get the line text
         Element lineElement = (Element) partElement.getElementsByTagName("line").item(0);
         String lineText = lineElement.getTextContent().trim();
     
         // Create Role object
-        return new Role(roleName, roleLevel, lineText, true);
+        return new Role(roleName, roleLevel, roleArea, lineText, true);
+    }
+
+    /**
+     * Parses an area from the given Element object.
+     * 
+     * @param areaElement
+     * @return Area
+     */
+    private Area parseArea(Element areaElement) {
+        int x = Integer.parseInt(areaElement.getAttribute("x"));
+        int y = Integer.parseInt(areaElement.getAttribute("y"));
+        int h = Integer.parseInt(areaElement.getAttribute("h"));
+        int w = Integer.parseInt(areaElement.getAttribute("w"));
+        return new Area(x, y, h, w);
     }
 
     /**
      * Returns the parsed scene cards.
      * 
-     * @return
+     * @return List<SceneCard>
      */
     public List<SceneCard> getCards() {
         return cards;
