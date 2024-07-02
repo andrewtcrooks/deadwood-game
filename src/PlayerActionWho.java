981091c9cf58d7be3038e8a5cc2dc2783cfed00b
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-07-02 13:04:16 -0700

    Created class PlayerActionWho

---
+++ src/PlayerActionWho.java
@@ -0,0 +1,37 @@
+public class PlayerActionWho implements PlayerAction{
+
+    /**
+     * Validates the who action for the player.
+     *
+     * @param player the player to who
+     * @param model the game model
+     * @param view the game view
+     * @return always returns true to validate who action
+     */
+    @Override
+    public boolean validate(Player player, GameModel model, GameView view) {
+        return true;
+    }
+
+    /**
+     * Executes the who action for the player.
+     *
+     * @param player the player
+     * @param model the game model
+     * @param view the game view
+     * @return always returns false to prevent player turn end
+     */
+    @Override
+    public boolean execute(Player player, GameModel model, GameView view) {
+        // Display the player info
+        view.displayPlayerInfo(
+                    player.getID(),
+                    player.getRank(),
+                    player.getMoney(),
+                    player.getCredits(),
+                    player.getRehearsalTokens()
+        );
+        return false;
+    }
+
+}
