e8d6acaeb51733a2b5d4db7ede7b14e1b99aadf0
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-07-04 13:47:40 -0700

    explicitly added public and private to methods and reorganized method order for clarity

--- src/Board.java
+++ src/Board.java
@@ -10,30 +10,165 @@ public class Board {
     private List<Player> players;
     private Deck deck;
     private Map<String, Location> locations;
+    private List<String> trailer_neighbors;
+    private Location trailer;
+    private List<String> office_neighbors;
+    private Location office;
     private String boardXMLFilePath;
-
 
     /**
      * Constructs a new Board with the specified number of days and players.
      *
      * @param numDays the number of days for the game
      * @param players the list of players in the game
+     * @param boardXMLFilePath the file path to the board XML file
+     * @param cardsXMLFilePath the file path to the cards XML file
      */
-    Board(int numDays, List<Player> players, String boardXMLFilePath, String cardsXMLFilePath) {
+    public Board(int numDays, List<Player> players, String boardXMLFilePath, String cardsXMLFilePath) {
         this.numDays = numDays;
         this.numScenesRemaining = 10;
         this.players = players;
         this.deck = new Deck(cardsXMLFilePath);
         this.locations = new HashMap<String, Location>();
         this.boardXMLFilePath = boardXMLFilePath;
+        // Initialize locations on the board
         initLocations(boardXMLFilePath);
+        this.trailer_neighbors = Arrays.asList("Main Street", "Saloon", "Hotel");
+        this.trailer = new Location("Trailer", this.trailer_neighbors, new Area(0,0,0,0), Arrays.asList(), Arrays.asList());
+        this.office_neighbors = Arrays.asList("Train Station", "Ranch", "Secret Hideout");
+        this.office = new Location("Casting Office", this.office_neighbors, new Area(0,0,0,0), Arrays.asList(), Arrays.asList());
+        this.locations.put("Trailer", this.trailer);
+        this.locations.put("Casting Office", this.office);
+        // Deal scene cards to locations
         dealSceneCardsToLocations();
     }
 
     /**
-     * Initializes the locations on the board.
+     * Returns the number of days remaining in the game.
+     *
+     * @return The number of days remaining in the game.
      */
-    void initLocations(String boardXMLFilePath) {
+    public int getNumDays() {
+        return this.numDays;
+    }
+
+    /**
+     * Decrements the number of days remaining in the game by 1.
+     *
+     * @return The new number of days remaining in the game.
+     */
+    public int decrementNumDays() {
+        return --this.numDays;
+    } 
+
+    /**
+     * Returns the number of scenes remaining in the game.
+     *
+     * @return The number of scenes remaining in the game.
+     */
+    public int getNumScenesRemaining() {
+        return this.numScenesRemaining;
+    }
+
+    /**
+     * Decrements the number of scenes remaining in the game by 1.
+     *
+     * @return The new number of scenes remaining in the game.
+     */
+    public int decrementNumScenesRemaining() {
+        return --this.numScenesRemaining;
+    }
+
+    /**
+     * Returns the locations on the board.
+     * 
+     * @return The locations on the board.
+     */
+    public Map<String, Location> getLocations() {
+        return locations;
+    }
+
+    /**
+     * Returns the number of shots remaining at a location.
+     *  
+     * @param location The location to check.
+     * @return The number of shots remaining at the location.
+     */
+    public int getLocationShotsRemaining(Location location) {
+        return location.getShots();
+    }
+
+    /**
+     * Wraps a shot at a location.
+     * 
+     * @param location The location to wrap a shot at.
+     */
+    public void wrapLocationShot(Location location) {
+        location.wrapShot();
+    }
+
+    /**
+     * Returns the neighbors of a location.
+     *
+     * @param location The location to check.
+     * @return The neighbors of the location.
+     */
+    public List<String> getLocationNeighbors(Location location) {
+        return location.getNeighbors();
+    }
+
+    /**
+     * Returns the roles at a location.
+     *
+     * @param location The location to check.
+     * @return The roles at the location.
+     */
+    public List<Role> getLocationRoles(Location location) {
+        return location.getRoles();
+    }
+
+    /**
+     * Returns the scene card at a location.
+     *
+     * @param location The location to check.
+     * @return The scene card at the location.
+     */
+    public SceneCard getLocationScene(Location location) {
+        return location.getSceneCard();
+    }
+
+    /**
+     * Resets the number of scenes remaining in the game to 10.
+     */
+    public void resetNumScenesRemaining() {
+        this.numScenesRemaining = 10;
+    }
+
+    /**
+     * Resets the board to its initial state.
+     */
+    public void resetBoard() {
+        initLocations(this.boardXMLFilePath);
+        dealSceneCardsToLocations();
+        resetPlayerLocations();
+    }
+
+    // return player object 
+    public Player getPlayer(int ID) {
+        for (Player player : players) {
+            if (Integer.valueOf(player.getID()).equals(ID)) {
+                return player;
+            }
+        }
+        throw new IllegalArgumentException("No player " + ID);
+    }
+
+    /**
+     * Initializes the locations on the board.
+     * 
+     * @param boardXMLFilePath The file path to the board XML file.
+     */
+    private void initLocations(String boardXMLFilePath) {
         ParseBoardXML parser = new ParseBoardXML();
         try {
             Document doc = parser.getDocFromFile(boardXMLFilePath);
@@ -45,103 +180,9 @@ public class Board {
     }
 
     /**
-     * Returns the number of days remaining in the game.
-     *
-     * @return The number of days remaining in the game.
-     */
-    int getNumDays() {
-        return this.numDays;
-    }
-
-    /**
-     * Decrements the number of days remaining in the game by 1.
-     *
-     * @return The new number of days remaining in the game.
-     */
-    int decrementNumDays() {
-        return --this.numDays;
-    } 
-
-    /**
-     * Returns the number of scenes remaining in the game.
-     *
-     * @return The number of scenes remaining in the game.
-     */
-    int getNumScenesRemaining() {
-        return this.numScenesRemaining;
-    }
-
-    /**
-     * Decrements the number of scenes remaining in the game by 1.
-     *
-     * @return The new number of scenes remaining in the game.
-     */
-    int decrementNumScenesRemaining() {
-        return --this.numScenesRemaining;
-    }
-
-    /**
-     * Returns the locations on the board.
-     * 
-     * @return The locations on the board.
-     */
-    Map<String, Location> getLocations() {
-        return locations;
-    }
-
-    /**
-     * Returns the number of shots remaining at a location.
-     *  
-     * @param location The location to check.
-     * @return The number of shots remaining at the location.
-     */
-    int getLocationShotsRemaining(Location location) {
-        return location.getShots();
-    }
-
-    /**
-     * Wraps a shot at a location.
-     * 
-     * @param location The location to wrap a shot at.
-     */
-    void wrapLocationShot(Location location) {
-        location.wrapShot();
-    }
-
-    /**
-     * Returns the neighbors of a location.
-     *
-     * @param location The location to check.
-     * @return The neighbors of the location.
-     */
-    List<String> getLocationNeighbors(Location location) {
-        return location.getNeighbors();
-    }
-
-    /**
-     * Returns the roles at a location.
-     *
-     * @param location The location to check.
-     * @return The roles at the location.
-     */
-    List<Role> getLocationRoles(Location location) {
-        return location.getRoles();
-    }
-
-    /**
-     * Returns the scene card at a location.
-     *
-     * @param location The location to check.
-     * @return The scene card at the location.
-     */
-    SceneCard getLocationScene(Location location) {
-        return location.getSceneCard();
-    }
-
-    /**
      * Deals new scene card to each location on the board.
      */
-    void dealSceneCardsToLocations() {
+    private void dealSceneCardsToLocations() {
         for (Location location : locations.values()) {
             SceneCard card = deck.drawCard();
             location.setSceneCard(card);
@@ -149,38 +190,14 @@ public class Board {
     }
 
     /**
-     * Resets the board to its initial state.
-     */
-    void resetBoard() {
-        initLocations(this.boardXMLFilePath);
-        dealSceneCardsToLocations();
-        resetPlayerLocations();
-    }
-
-    // return player object 
-    Player getPlayer(int ID) {
-        for (Player player : players) {
-            if (Integer.valueOf(player.getID()).equals(ID)) {
-                return player;
-            }
-        }
-        throw new IllegalArgumentException("No player " + ID);
-    }
-
-    /**
      * Change all player locations on the board to "Trailer"
      */
-    void resetPlayerLocations() {
+    private void resetPlayerLocations() {
         for (Player player : players) {
             player.setLocation(locations.get("trailer"));
         }
     }
 
-    /**
-     * Resets the number of scenes remaining in the game to 10.
-     */
-    void resetNumScenesRemaining() {
-        this.numScenesRemaining = 10;
-    }
+
 
 }
