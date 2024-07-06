5fbf9218a0891b3f180b43b5a7313cfdca5d7581
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-07-05 17:46:38 -0700

    cleaned up repetitive code

--- src/PlayerActionHelp.java
+++ src/PlayerActionHelp.java
@@ -27,20 +27,20 @@ public class PlayerActionHelp implements PlayerAction {
     @Override
     public boolean execute(Player player, GameModel model, GameView view) {
         // Display the player's options
-        view.showMessage("You can enter one of the following commands:");
-        view.showMessage("who: Display player info");
-        view.showMessage("where: Display player location");
-        view.showMessage("board: Display all player locations and indicate the active player");
-        view.showMessage("move: Move player to a new location");
-        view.showMessage("work: Player works on a part");
-        view.showMessage("upgrade: Player upgrades rank");
-        view.showMessage("rehearse: Player rehearses");
-        view.showMessage("act: Player acts on a role");
-        view.showMessage("end: End player's turn");
-        view.showMessage("quit: Quit the game");
-        view.showMessage("save: Save the game");
-        view.showMessage("load: Load the game");
-        view.showMessage("help: Display player options");
+        view.showMessage("You can enter one of the following commands:\n" +
+                         "who: Display player info\n" +
+                         "where: Display player location\n" +
+                         "board: Display all player locations and indicate the active player\n" +
+                         "move: Move player to a new location\n" +
+                         "work: Player works on a part\n" +
+                         "upgrade: Player upgrades rank\n" +
+                         "rehearse: Player rehearses\n" +
+                         "act: Player acts on a role\n" +
+                         "end: End player's turn\n" +
+                         "quit: Quit the game\n" +
+                         "save: Save the game\n" +
+                         "load: Load the game\n" +
+                         "help: Display player options");
         return false;
     }
 
