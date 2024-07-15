3d5f039e511705160e4ccc0c7e24432e66bae9ad
Andrew T. Crooks <andrewtcrooks@gmail.com>
2024-07-15 15:15:31 -0700

    updated comments

--- src/GameView.java
+++ src/GameView.java
@@ -1,8 +1,9 @@
+/**
+ * GameView interface
+ */
 public interface GameView extends Observer{
-
     void update(Object arg);
     int getNumPlayers();
     String getPlayerInput();
     void showMessage(String message);
-
 }
